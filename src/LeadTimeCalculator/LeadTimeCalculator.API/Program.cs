6135bd78bddda3cf8e630cbf44647e0e0243ad7b
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2024-11-25 13:02:10 +0100

    API: Added Scalar for exploring endpoints and models

--- src/LeadTimeCalculator/LeadTimeCalculator.API/Program.cs
+++ src/LeadTimeCalculator/LeadTimeCalculator.API/Program.cs
@@ -1,5 +1,6 @@
 using LeadTimeCalculator.API.Features.WorkdayCalendarFeature;
 using LeadTimeCalculator.API.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,43 +12,21 @@ builder.Services.AddWorkdayCalendarFeature();
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
-var apiEndpointGroup = app.MapGroup("api");
+var apiEndpointGroup = app
+    .MapGroup("api")
+    .WithOpenApi();
 
 apiEndpointGroup.AddWorkdayCalendarFeatureEndpoints();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapGet("/", () => TypedResults.Redirect("/scalar/v1"));
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 public partial class Program { }
